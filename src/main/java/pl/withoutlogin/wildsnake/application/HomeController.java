3a87263a4b754a5791997e785d67d732193d06de
Szymon <predefined77@gmail.com>
2015-10-12 09:13:55 +0200

    SNAKE-1 | landing

--- src/main/java/pl/withoutlogin/wildsnake/application/HomeController.java
+++ src/main/java/pl/withoutlogin/wildsnake/application/HomeController.java
@@ -1,7 +1,14 @@
 package pl.withoutlogin.wildsnake.application;
 
-/**
- * Created by Szymon on 12/10/15.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+
 }
