a687ccd24b235206c05fc086e011f46175109905
jasphall <maciekksikorski@gmail.com>
2015-10-12 18:09:47 +0200

    readme

--- src/test/java/pl/project/WildApplicationTests.java
+++ src/test/java/pl/project/WildApplicationTests.java
@@ -2,6 +2,9 @@ package pl.project;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,8 +14,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @WebAppConfiguration
 public class WildApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void should_show_main_page() {
+        // when
+        ResponseEntity<String> entity = template.getForEntity("http://localhost:8080/", String.class);
+
+        // then
+        assert entity.getStatusCode() == HttpStatus.OK;
+        assert entity.getBody().contains("coming soon");
+    }
+
+    TestRestTemplate template = new TestRestTemplate();
 
 }
