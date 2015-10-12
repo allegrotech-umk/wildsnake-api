d1f6868ed95c71a0e08ce7cf69b69c876eb8e949
blackbat13 <blackbat13@gmail.com>
2015-10-12 09:12:52 +0200

    First, initial commit.

---
+++ src/main/java/com/blackbat13/wildsnake/WildsnakeApplication.java
@@ -0,0 +1,12 @@
+package com.blackbat13.wildsnake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildsnakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildsnakeApplication.class, args);
+    }
+}
