c5c5b54271dfc1cb863f30bd867982c6e1d3302f
Lukasz <kukol3@gmail.com>
2015-10-18 21:27:02 +0200

    new files from master

---
+++ src/main/java/pl/jwest/wildsnake/application/HomeController.java
@@ -0,0 +1,14 @@
+package pl.jwest.wildsnake.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+
+}
