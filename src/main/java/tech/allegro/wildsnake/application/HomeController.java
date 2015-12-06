df49e9eb9a0801222964d5e3f017e87456678e57
Michał Jezierski <ambinanitelo@gmail.com>
2015-12-06 19:56:10 +0100

    js first steps

--- src/main/java/tech/allegro/wildsnake/application/HomeController.java
+++ src/main/java/tech/allegro/wildsnake/application/HomeController.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tech.allegro.wildsnake.showcase.model.ShowcaseItem;
 import tech.allegro.wildsnake.showcase.service.ShowcaseService;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -25,6 +29,12 @@ public class HomeController {
         model.addAttribute("pageMessage", pageMessage);
         model.addAttribute("showcaseItems", showcaseService.getItems());
         return "index";
+    }
+
+    @RequestMapping("/products")
+    @ResponseBody
+    public List<ShowcaseItem> products() {
+        return showcaseService.getItems();
     }
 
 }
---
+++ src/main/resources/static/scripts/main.js
@@ -0,0 +1,24 @@
+(function (document, window) {
+  'use strict';
+
+  let container = document.querySelector(".products");
+
+  function insert(object) {
+    let productElement = document.createElement("li");
+    productElement.innerHTML = object.title;
+    container.appendChild(productElement);
+  }
+
+  function getProducts() {
+    window.fetch("/products")
+      .then((response) => {
+        return response.json();
+      })
+      .then((products) => {
+        products.forEach(insert);
+      });
+  }
+
+  document.addEventListener("DOMContentLoaded", getProducts);
+
+})(document, window);
