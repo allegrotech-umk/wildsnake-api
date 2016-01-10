ea8be115413063e82413263daa784bdfbf98bb40
Szymon <predefined77@gmail.com>
2016-01-10 22:56:24 +0100

    some small css improvements

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,5 +1,5 @@
 function wildsnakeCtrl($http) {
-  $http.get('/api/v1/products?limit=4').then(response => this.products = response.data);
+  $http.get('/api/v1/products').then(response => this.products = response.data);
 }
 
 function wildsnakeConfig ($interpolateProvider) {
