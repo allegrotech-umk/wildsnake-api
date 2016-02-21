3e18e79401be4a6b62e40f7316888144c1c6a494
sdomeracki <szymondomeracki@outlook.com>
2016-02-21 20:15:22 +0100

    productCategory entity

--- src/main/java/tech/allegro/wildsnake/rest/ProductsEndPoint.java
+++ src/main/java/tech/allegro/wildsnake/rest/ProductsEndPoint.java
@@ -3,13 +3,7 @@ package tech.allegro.wildsnake.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.allegro.wildsnake.model.ProductDomain;
 import tech.allegro.wildsnake.product.service.ProductService;
 
@@ -64,9 +58,9 @@ public class ProductsEndPoint {
     )
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void addProduct(
-            @RequestBody ProductDomain productDomain
+            @RequestBody ProductDomain productDomain, @RequestBody String productCategoryName
     ) {
-        productService.createUniqueProduct(productDomain);
+        productService.createUniqueProduct(productDomain, productCategoryName);
     }
 
     @RequestMapping(
